9d4ae53e53e80116b17abf6d939600a754721eb5
mcqeems <mcqeemsofficial@gmail.com>
2025-09-16 22:30:30 +0700

    reverted

--- auth.ts
+++ auth.ts
@@ -3,7 +3,7 @@ import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
-// import bcrypt from 'bcrypt';
+import bcrypt from 'bcrypt';
 import postgres from 'postgres';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
@@ -31,9 +31,8 @@ export const { auth, signIn, signOut } = NextAuth({
           const { email, password } = parsedCredentials.data;
           const user = await getUser(email);
           if (!user) return null;
-          // const passwordsMatch = await bcrypt.compare(password, user.password);
-          // if (passwordsMatch)
-          return user;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (passwordsMatch) return user;
         }
         return null;
       },
