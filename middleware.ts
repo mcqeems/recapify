44ae1def1f2c2bec5187af6b4a9820d4c30588ef
mcqeems <mcqeemsofficial@gmail.com>
2025-09-16 22:26:10 +0700

    experimental edge

--- middleware.ts
+++ middleware.ts
@@ -7,5 +7,5 @@ export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
   // runtime: 'nodejs',
-  runtime: 'edge',
+  runtime: 'experimental-edge',
 };
