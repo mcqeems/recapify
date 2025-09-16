ce2993e5026cf058092ee697ec8cc7d9c2edbce3
mcqeems <mcqeemsofficial@gmail.com>
2025-09-16 21:14:08 +0700

    fixed edit customer

--- app/dashboard/customers/[id]/edit/page.tsx
+++ app/dashboard/customers/[id]/edit/page.tsx
@@ -2,23 +2,27 @@ import Form from '@/app/ui/customers/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomerById } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
 
-export default async function Page({ params }: { params: { id: string } }) {
+export const metadata: Metadata = {
+  title: `Edit Customer`,
+};
+
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   const id = params.id;
-  const customer = await fetchCustomerById(id);
-
+  const [customer] = await Promise.all([fetchCustomerById(id)]);
   if (!customer) {
     notFound();
   }
-
   return (
     <main>
       <Breadcrumbs
         breadcrumbs={[
-          { label: 'Customers', href: '/dashboard/customers' },
+          { label: 'Invoices', href: '/dashboard/invoices' },
           {
-            label: 'Edit Customer',
-            href: `/dashboard/customers/${id}/edit`,
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
             active: true,
           },
         ]}
